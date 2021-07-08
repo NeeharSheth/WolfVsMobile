d7db188eb9e1eadc8b86c5ee8d4f3400e836b12e
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-07-08 15:39:43 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var images=["pencil.png","knife.JPEG","binder.jpeg","blender.jpeg","penstand.jpg","car.jpeg"];
+var images=["pencil.png","knife.jpeg","binder.jpeg","blender.jpeg","penstand.jpg","car.jpeg"];
 var azzure_result=["pencil","scalpel","binder","blender","toothpick","coupe"];
+var mobile_result=["crane","cleaver","binder"," pencil sharpener","file","sports car"]
+var conclusion_result=["Wolfram is more accurate than MobileNet","MobileNet is more accurate than Wolfram","Both are equally accurate","Wolfram is more accurate than MobileNet","None of them are accurate","Wolfram is more accurate than MobileNet"];
 var image_no= 0;
 
 function change_image(){
@@ -14,9 +16,14 @@ document.getElementById("test_image").src= images[image_no];
 }
 
 function check(){
-    result= azzure_result[image_no];
-    console.log(result);
-document.getElementById("wolfram_results").innerHTML= result;
+    azzure_results= azzure_result[image_no];
+    mobilenet_results= mobile_result[image_no];
+    conclusion_results= conclusion_result[image_no];
+    console.log(azzure_result);
 document.getElementById("azzure_results_div").style.visibility= "visible";
 document.getElementById("mobilenet_results_div").style.visibility= "visible";
+document.getElementById("conclusion_results_div").style.visibility= "visible";
+document.getElementById("wolfram_results").innerHTML= azzure_results;
+document.getElementById("mobilenet_results").innerHTML= mobilenet_results;
+document.getElementById("conclusion_results").innerHTML= conclusion_results;
 }
